d60b080719d7976eb503f0a956001c469a073b68
Benjamin Chimoy <benjaminchimoy@Benjamins-MBP.home>
2020-05-14 17:14:55 +0200

    more ordered

--- moods-of-berlin/client/src/App.js
+++ moods-of-berlin/client/src/App.js
@@ -1,39 +1,17 @@
-import React, {Component, Fragment} from 'react';
-import './App.css';
-import places from "./placesFrontend.json"
-import PlacesList from "./PlacesList"
-import Search from "./Search.js"
+import React, { Component, Fragment } from "react";
+import Places from "./components/Places.js";
+
 
 class App extends Component {
-
-  state = {
-    places: places,
-    query: "",
-  };
-
-  setQuery = (query) => {
-    this.setState({
-      query: query,
-    });
-  };
-
-  render() {
-
-    return (
-      
-      <Fragment>
-      <Search query={this.state.query} triggerSetQuery={this.setQuery} />
-      
-      <PlacesList places={this.state.places} query={this.state.query} />
-
-      </Fragment>
-      
-    );
-
-
-
-  }
   
+  render() {
+    return (
+      <Fragment>
+       
+        <Places/>
+      </Fragment>
+    );
+  }
 }
 
 export default App;
