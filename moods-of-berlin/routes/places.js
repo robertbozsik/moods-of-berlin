60c74e79173979ccc0feaef8eb77e87e5064f241
robertbozsik <robertbozsik@gmail.com>
2020-05-14 14:22:32 +0200

    changing the schema

--- moods-of-berlin/routes/places.js
+++ moods-of-berlin/routes/places.js
@@ -2,20 +2,35 @@ const express = require("express");
 const router = express.Router();
 const Place = require("../models/Place");
 
+// create a place
 router.post("/", (req, res) => {
-  const title = req.body.title;
-  const description = req.body.description;
-  const mood = req.body.mood;
-  const imgPath = req.body.imgPath;
-  const address = req.body.address;
-  const review = req.body.review;
+  //   const title = req.body.title;
+  //   const description = req.body.description;
+  //   const mood = req.body.mood;
+  //   const imgPath = req.body.imgPath;
+  //   const street = req.body.street;
+  //   const zip = req.body.zip;
+  //   const city = req.body.city;
+  //   const review = req.body.review;
+  const {
+    title,
+    description,
+    mood,
+    imgPath,
+    street,
+    zip,
+    city,
+    review,
+  } = req.body;
 
   Place.create({
     title,
     description,
     mood,
     imgPath,
-    address,
+    street,
+    zip,
+    city,
     review,
   })
     .then((place) => {
@@ -25,5 +40,18 @@ router.post("/", (req, res) => {
       res.json(err);
     });
 });
+
+// return all the places
+router.get("/", (req, res) => {
+  Place.find()
+    .then((places) => {
+      res.status(200).json(places);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+// return the specified place
 
 module.exports = router;
