0c72170b23dd20afa645c4f536f2efa60d4c4222
robertbozsik <robertbozsik@gmail.com>
2020-05-15 20:31:09 +0200

    Benjamin: map and colorbar

---
+++ moods-of-berlin/client/src/components/Colorbar.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Colorbar = (props) => {
+  return (
+    <div>
+      <body>
+        <div class="container">
+          <div class="box">joyful</div>
+          <div class="box">relaxed</div>
+          <div class="box">energetic</div>
+          <div class="box">bored</div>
+          <div class="box">thoughtful</div>
+          <div class="box">melancholic</div>
+          <div class="box">anxious</div>
+          <div class="box">angry</div>
+        </div>
+      </body>
+    </div>
+  );
+};
+
+export default Colorbar;
