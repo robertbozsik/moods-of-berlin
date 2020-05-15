23b2eb110e734de5fb476c4aaede03075c8b9033
Benjamin Chimoy <benjaminchimoy@Benjamins-MBP.home>
2020-05-15 14:53:58 +0200

    colorbar basic

--- moods-of-berlin/client/src/App.js
+++ moods-of-berlin/client/src/App.js
@@ -1,13 +1,16 @@
 import React, { Component, Fragment } from "react";
 import Places from "./components/Places.js";
 import Navbar from "./components/Navbar.js";
+import { Route} from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <Fragment>
-        <Navbar />
-        <Places />
+              <Navbar />
+        <Route exact path='/places'
+          component={Places}
+        />
       </Fragment>
     );
   }
--- moods-of-berlin/client/src/components/Places.js
+++ moods-of-berlin/client/src/components/Places.js
@@ -1,39 +1,42 @@
 import React, { Component, Fragment } from "react";
-import 'bootstrap/dist/css/bootstrap.css'
+import "bootstrap/dist/css/bootstrap.css";
 import places from "../placesFrontend.json";
 import PlacesList from "./Placeslist";
 import Search from "./Search.js";
-import axios from 'axios';  // not used 
+import axios from "axios"; // not used
+import Colorbar from "./Colorbar";
 
 class App extends Component {
   state = {
     places: places, // should be [] after conecting to backend
     query: "",
+    mood: {
+rage: false,
+bored: false,
+    },
   };
 
-// not used - >
+  // not used - >
 
   componentDidMount = () => {
     this.getData();
-  }
+  };
 
   getData = () => {
     axios
-    .get('/api/places')
-    .then(response => {
-      console.log(response.data)
-      this.setState({
-        places: response.data
+      .get("/api/places")
+      .then((response) => {
+        console.log(response.data);
+        this.setState({
+          places: response.data,
+        });
       })
-    })
-.catch(err => {
-  console.log (err)
-})
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
-  }
-
-
-  // < - not used 
+  // < - not used
 
   setQuery = (query) => {
     this.setState({
@@ -41,9 +44,18 @@ class App extends Component {
     });
   };
 
+  setMood = (mood) => {
+    this.setState({
+      mood: mood,
+    });
+  };
+
   render() {
+   
+
     return (
       <Fragment>
+        <Colorbar mood="rage" triggerSetMood={this.setMood} />
         <Search query={this.state.query} triggerSetQuery={this.setQuery} />
         <PlacesList places={this.state.places} query={this.state.query} />
       </Fragment>
