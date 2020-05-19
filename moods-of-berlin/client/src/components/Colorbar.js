4c3fe5888cd72d15678041d5e1aa65da2b16498a
jesuscabrera <chuscabrera@gmail.com>
2020-05-19 12:04:31 +0200

    solving github banana conflicts

--- moods-of-berlin/client/src/components/Colorbar.js
+++ moods-of-berlin/client/src/components/Colorbar.js
@@ -3,7 +3,6 @@ import "./Colorbar.css";
 
 const Colorbar = (props) => {
   return (
-<<<<<<< HEAD
     <div className="container">
       <div className="box">joyful</div>
       <div className="box">relaxed</div>
@@ -13,19 +12,6 @@ const Colorbar = (props) => {
       <div className="box">melancholic</div>
       <div className="box">anxious</div>
       <div className="box">angry</div>
-=======
-    <div>
-      <div className="container">
-        <div className="box">joyful</div>
-        <div className="box">relaxed</div>
-        <div className="box">energetic</div>
-        <div className="box">bored</div>
-        <div className="box">thoughtful</div>
-        <div className="box">melancholic</div>
-        <div className="box">anxious</div>
-        <div className="box">angry</div>
-      </div>
->>>>>>> 73d2b5c4054f9ccd03fe72d7d7ac4c1a60385b15
     </div>
   );
 };
