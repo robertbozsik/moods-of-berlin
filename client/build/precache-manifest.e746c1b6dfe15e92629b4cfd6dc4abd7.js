07c4d16adc4efc21be382b7c5f578b71437fd8da
jesuscabrera <chuscabrera@gmail.com>
2020-05-21 14:55:51 +0200

    build for deployment heroku

---
+++ client/build/precache-manifest.e746c1b6dfe15e92629b4cfd6dc4abd7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5a166cd4f0bc1728ebbd40fa7fe6b8b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3d87c7bde4b7a858742e",
+    "url": "/static/css/2.db6f3515.chunk.css"
+  },
+  {
+    "revision": "fbebaf404142eb79911c",
+    "url": "/static/css/main.aac466f3.chunk.css"
+  },
+  {
+    "revision": "3d87c7bde4b7a858742e",
+    "url": "/static/js/2.db35a38c.chunk.js"
+  },
+  {
+    "revision": "ebec6ec5880fcec562f9a7747ae47b51",
+    "url": "/static/js/2.db35a38c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fbebaf404142eb79911c",
+    "url": "/static/js/main.d07194d2.chunk.js"
+  },
+  {
+    "revision": "0e35831b56fb2c044e12",
+    "url": "/static/js/runtime-main.98a2b520.js"
+  }
+]);
