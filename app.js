300c32e2ca44c9149a833a3ef8db8620a95eb40e
jesuscabrera <chuscabrera@gmail.com>
2020-05-21 14:59:06 +0200

    changes in app.js

--- app.js
+++ app.js
@@ -68,7 +68,8 @@ app.use(
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "hbs");
-app.use(express.static(path.join(__dirname, "public")));
+// app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "/client/build")));
 app.use(favicon(path.join(__dirname, "public", "images", "favicon.ico")));
 
 // default value for title local
@@ -88,5 +89,10 @@ app.locals.title = "Express - Generated with IronGenerator";
 
 app.use("/api/places", require("./routes/places"));
 app.use("/api/auth", require("./routes/auth"));
+
+app.use((req, res) => {
+  // If no routes match, send them the React HTML.
+  res.sendFile(__dirname + "/client/build/index.html");
+});
 
 module.exports = app;
